11aacad679c783b682ef689c14992dec41c662b6
kaanf <frtpkaan@gmail.com>
2026-03-14 20:12:20 +0100

    build(lint): add ktlint to the gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,16 @@
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+import org.jlleitschuh.gradle.ktlint.tasks.BaseKtLintCheckTask
+import org.jlleitschuh.gradle.ktlint.tasks.KtLintFormatTask
+
+fun BaseKtLintCheckTask.excludeGeneratedSources() {
+    exclude("**/build/**")
+    exclude("**/generated/**")
+    exclude { fileTreeElement ->
+        val normalizedPath = fileTreeElement.file.invariantSeparatorsPath
+        normalizedPath.contains("/build/") || normalizedPath.contains("/generated/")
+    }
+}
+
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
@@ -12,4 +25,43 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.room) apply false
+    alias(libs.plugins.ktlint)
+}
+
+configure<KtlintExtension> {
+    outputToConsole.set(true)
+    ignoreFailures.set(false)
+}
+
+tasks.withType<BaseKtLintCheckTask>().configureEach {
+    excludeGeneratedSources()
+}
+
+tasks.withType<KtLintFormatTask>().configureEach {
+    excludeGeneratedSources()
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    extensions.configure<KtlintExtension> {
+        outputToConsole.set(true)
+        ignoreFailures.set(false)
+    }
+
+    tasks.withType<BaseKtLintCheckTask>().configureEach {
+        excludeGeneratedSources()
+    }
+
+    tasks.withType<KtLintFormatTask>().configureEach {
+        excludeGeneratedSources()
+    }
+}
+
+tasks.named("ktlintCheck") {
+    dependsOn(subprojects.map { "${it.path}:ktlintCheck" })
+}
+
+tasks.named("ktlintFormat") {
+    dependsOn(subprojects.map { "${it.path}:ktlintFormat" })
 }
